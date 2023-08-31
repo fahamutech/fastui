b0b24879f7948086624b14e226b70a75403cbe40
joshuamshana <mama27j@gmail.com>
2023-08-31 15:20:36 +0300

    Implement get specs for single yaml file

--- src/index.mjs
+++ src/index.mjs
@@ -3,14 +3,22 @@ import * as yaml from "js-yaml"
 import {readFile} from 'node:fs/promises'
 
 export async function readSpecs(rootFolder) {
+    if (`${rootFolder}`.endsWith('.yml')) {
+        const rootParts = `${rootFolder}`.split('/');
+        const rootFileName = rootParts.pop();
+        const pattern = `${rootParts.join('/')}/**/${rootFileName.replace('.yml', '')}.yml`;
+        // console.log(pattern, 'FILENAME');
+        return await glob(pattern, {
+            ignore: ['**/node_modules/**']
+        });
+    }
     const root = rootFolder === '/' ? './' : rootFolder?.endsWith('/') ? rootFolder : `${rootFolder ?? '.'}/`;
-    // console.log(root,'ROOOT');
     return await glob(`${root}**/*.yml`, {
         ignore: ['**/node_modules/**']
     });
 }
 
 export async function specToJSON(specPath) {
-    return yaml.load(await readFile(specPath, {encoding: 'utf-8'}),{});
+    return yaml.load(await readFile(specPath, {encoding: 'utf-8'}), {});
 }
 
