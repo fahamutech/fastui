43ff5e1301245d4bcc098a5f9b7a06bab3b98d7b
joshuamshana <mama27j@gmail.com>
2023-09-06 01:17:50 +0300

    update specs match

--- src/services/specs.mjs
+++ src/services/specs.mjs
@@ -22,7 +22,7 @@ export async function readSpecs(unParsedRootFolder) {
         : rootFolder?.endsWith(pathSep)
             ? rootFolder
             : `${rootFolder ?? '.'}/`;
-    const pattern = `${root.replace(pathSep,'/')}**/*.yml`;
+    const pattern = `${root.replaceAll(pathSep,'/')}**/*.yml`;
     console.log(pattern,'----PATTERN----')
     return await glob(pattern, {
         ignore: ['**/node_modules/**']
